52936caa12d3b33f8d9fdb6ddf4d94128c41921d
Grahambone37 <electricgrunt37@gmail.com>
2022-11-28 18:37:45 -0800

    set up stuff

---
+++ script.js
@@ -0,0 +1,17 @@
+/*button event listeners*/
+let coffeeButton = document.querySelector('#coffee-button')
+let restaurantButton = document.querySelector('#restaurant-button')
+let hotelButton = document.querySelector('#hotel-button')
+let marketButton = document.querySelector('#market-button')
+
+/*set up Leaflet map*/
+
+
+/*obtain users current location*/
+
+
+/*map userLocation on Leaflet map*/
+
+
+/*business type locations*/
+
